c6c10d0b2704bdcd14a518eb0cc198ae09845253
Gedas <gediminas.repecka@gmail.com>
2023-02-23 22:52:05 +0200

    Updated .gitignore to ignore certain files/directories

--- OOPIntro/src/Main.java
+++ OOPIntro/src/Main.java
@@ -1,11 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        Student S = new Student();
-        S.setName("Bob");
-        String name = S.getName();
-        Student S2 = new Student("Alice");
-        System.out.println("Students name is: " + name);
-        name = S2.getName();
-        System.out.println("Students name is: " + name);
+        Student S1 = new Student(0, "Alice");
+        Student S2 = new Student(1, "Bob");
+        Student S3 = new Student(2, "John", "Smith", "Java");
+        Student S4 = new Student(3, "Jeff", "Johnson", "Java");
+        Student S5 = new Student();
+        S1.setSurname("Smith");
+        S1.setGroup("Java");
+        S2.setSurname("Johnson");
+        S2.setGroup("C");
+        S5.setId(4);
+        S5.setName("Jonas");
+        S5.setSurname("Petraitis");
+        S5.setGroup("C++");
+        Student[] students = {S1, S2, S3, S4, S5};
+        printObjectAttributes(students);
+        S1.setName("Robinson");
+        S2.setSurname("Martingale");
+        S3.setGroup("Python");
+        S4.setId(6);
+        S5.setName("Petras");
+        System.out.println("###############################################################");
+        printObjectAttributes(students);
+
+    }
+
+    static void printObjectAttributes(Student[] students) {
+        for (Student S : students) {
+            int id = S.getId();
+            String name = S.getName();
+            String surname = S.getSurname();
+            String group = S.getGroup();
+            System.out.println("Introduction of student " + id + ":\n"
+                    + "Hi, my name is " + name + " " + surname + ", and i am studying " + group + "\n"
+                    + "________________________________________");
+        }
     }
 }
--- OOPIntro/src/Student.java
+++ OOPIntro/src/Student.java
@@ -1,24 +1,51 @@
 public class Student { // new class named 'Student'
-    private String group; // class variable group
+    private int id;
     private String name; // class variable name
-    public Student () { // if we don't have an empty constructor we would not be able to create a new object without parameters, it's best practice to have one
-
+    private String surname;
+    private String group; // class variable group
+    public Student () {
+        /*
+         if we don't have an empty constructor we would not be able to create a new object without parameters,
+         therefore it's best practice to have one.
+         */
     }
-    public Student(String name, String group) { // name of the constructor must always match a name of the class
+    public Student(int id, String name, String surname, String group) { // name of the constructor must always match a name of the class
+        this.id = id;
         this.name = name;
+        this.surname = surname;
         this.group = group;
     }
 
-    public Student(String name) { // we can have multiple constructors,
+    public Student(int id) { // we can have multiple constructors with different parameters.
+        this.id = id;
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
         this.name = name;
     }
 
+    public void setId(int id) { // setter method for id
+        this.id = id;
+    }
+
+    public int getId() { // getter method for id
+        return this.id;
+    }
     public void setName(String name) { // setter method for name
         this.name = name;
     }
 
     public String getName() { // getter method for name
         return this.name;
+    }
+
+    public void setSurname(String surname) { // setter method for surname
+        this.surname = surname;
+    }
+
+    public String getSurname() { // getter method for surname
+        return this.surname;
     }
 
     public void setGroup(String group) {
